d038b5ec691ae5bedac6b6b28e0be13a2783bd11
naraekwon86 <wde0530@gmail.com>
2024-01-25 18:08:22 +0900

    2-05

--- src/test/java/com/ll/Sbb3ApplicationTests.java
+++ src/test/java/com/ll/Sbb3ApplicationTests.java
@@ -1,13 +1,43 @@
 package com.ll;
 
+import com.ll.sbb3.Answer;
+import com.ll.sbb3.AnswerRepository;
+import com.ll.sbb3.Question;
+import com.ll.sbb3.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class Sbb3ApplicationTests {
 
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Transactional
     @Test
-    void contextLoads() {
+    void testJpa() {
+       Optional<Question> oq = this.questionRepository.findById(2);
+       assertTrue(oq.isPresent());
+       Question q = oq.get();
+
+       List<Answer> answerList = q.getAnswerList();
+
+       assertEquals(1,answerList.size());
+       assertEquals("네 자동으로 생성됩니다." , answerList.get(0).getContent());
+
+
     }
 
 }
